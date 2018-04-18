81164ea8484822ac15a4e5225da0c5d3fe57aac4
Gustavo Dias de Oliveira <gust.dias@gmail.com>
2018-04-18 15:49:44 -0300

    Created a random sample VRP inputs generator, for tests

---
+++ vrp-sample-gen.py
@@ -0,0 +1,22 @@
+# encoding: utf-8
+import sys
+import random
+import math
+
+
+nodescount = int(sys.argv[1])
+maxcap = float(sys.argv[2])
+minX = float(sys.argv[3])
+maxX = float(sys.argv[4])
+minY = float(sys.argv[5])
+maxY = float(sys.argv[6])
+
+
+print 'params:'
+print '  capacity %f' % maxcap
+print 'nodes:'
+for i in range(nodescount):
+	demand = random.uniform(0.0, maxcap)
+	x = random.uniform(minX, maxX)
+	y = random.uniform(minY, maxY)
+	print ('  node%0' + str(math.ceil(math.log(nodescount + 1) / math.log(10))) + 'd\t\t%.3f\t\t%.3f\t\t%.3f') % (i+1, demand, x, y)
