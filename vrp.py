7d818614646b1f7a72dae5f0343f535fa3ac5f18
Gustavo Dias de Oliveira <gust.dias@gmail.com>
2018-04-19 09:11:58 -0300

    Documenting the VRP.py code with comments, and doing some adjusts on "adjust" function

--- vrp.py
+++ vrp.py
@@ -7,6 +7,9 @@ import math
 vrp = {}
 
 
+## First reading the VRP from the input ##
+
+
 def readinput():
 	try:
 		line = raw_input().strip()
@@ -64,17 +67,23 @@ if len(vrp['nodes']) == 0:
 	exit(1)
 
 
+## After inputting and validating it, now computing the algorithm ##
+
+
 def distance(n1, n2):
 	dx = n2['posX'] - n1['posX']
 	dy = n2['posY'] - n1['posY']
 	return math.sqrt(dx * dx + dy * dy)
 
 def fitness(p):
+	# The first distance is from depot to the first node of the first route
 	s = distance(vrp['nodes'][0], vrp['nodes'][p[0]])
+	# Then calculating the distances between the nodes
 	for i in range(len(p) - 1):
 		prev = vrp['nodes'][p[i]]
 		next = vrp['nodes'][p[i + 1]]
 		s += distance(prev, next)
+	# The last distance is from the last node of the last route to the depot
 	s += distance(vrp['nodes'][p[len(p) - 1]], vrp['nodes'][0])
 	return s
 
@@ -82,7 +91,7 @@ def adjust(p):
 	# Adjust repeated
 	repeated = True
 	while repeated:
-		c = False
+		repeated = False
 		for i1 in range(len(p)):
 			for i2 in range(i1):
 				if p[i1] == p[i2]:
@@ -94,10 +103,9 @@ def adjust(p):
 							break
 					if haveAll:
 						del p[i1]
-					c = True
-				if c: break
-			if c: break
-		repeated = c
+					repeated = True
+				if repeated: break
+			if repeated: break
 	# Adjust capacity exceed
 	i = 0
 	s = 0.0
@@ -109,7 +117,7 @@ def adjust(p):
 			s = 0.0
 		i += 1
 	i = len(p) - 2
-	# adjust two consective depots
+	# Adjust two consective depots
 	while i >= 0:
 		if p[i] == 0 and p[i + 1] == 0:
 			del p[i]
@@ -121,37 +129,47 @@ iterations = int(sys.argv[2])
 
 pop = []
 
+# Generating random initial population
 for i in range(popsize):
 	p = range(1, len(vrp['nodes']))
 	random.shuffle(p)
 	pop.append(p)
-
 for p in pop:
 	adjust(p)
 
+# Running the genetic algorithm
 for i in range(iterations):
 	nextPop = []
+	# Each one of this iteration will generate two descendants individuals. Therefore, to guarantee same population size, this will iterate half population size times
 	for j in range(int(len(pop) / 2)):
+		# Selecting randomly 4 individuals to select 2 parents by a binary tournament
 		parentIds = set()
 		while len(parentIds) < 4:
 			parentIds |= {random.randint(0, len(pop) - 1)}
 		parentIds = list(parentIds)
+		# Selecting 2 parents with the binary tournament
 		parent1 = pop[parentIds[0]] if fitness(pop[parentIds[0]]) < fitness(pop[parentIds[1]]) else pop[parentIds[1]]
 		parent2 = pop[parentIds[2]] if fitness(pop[parentIds[2]]) < fitness(pop[parentIds[3]]) else pop[parentIds[3]]
+		# Selecting two random cutting points for crossover, with the same points (indexes) for both parents, based on the shortest parent
 		cutIdx1, cutIdx2 = random.randint(1, min(len(parent1), len(parent2)) - 1), random.randint(1, min(len(parent1), len(parent2)) - 1)
 		cutIdx1, cutIdx2 = min(cutIdx1, cutIdx2), max(cutIdx1, cutIdx2)
+		# Doing crossover and generating two children
 		child1 = parent1[:cutIdx1] + parent2[cutIdx1:cutIdx2] + parent1[cutIdx2:]
 		child2 = parent2[:cutIdx1] + parent1[cutIdx1:cutIdx2] + parent2[cutIdx2:]
 		nextPop += [child1, child2]
+	# Doing mutation: swapping two positions in one of the individuals, with 1:15 probability
 	if random.randint(1, 15) == 1:
 		ptomutate = nextPop[random.randint(0, len(nextPop) - 1)]
 		i1 = random.randint(0, len(ptomutate) - 1)
 		i2 = random.randint(0, len(ptomutate) - 1)
 		ptomutate[i1], ptomutate[i2] = ptomutate[i2], ptomutate[i1]
+	# Adjusting individuals
 	for p in nextPop:
 		adjust(p)
+	# Updating population generation
 	pop = nextPop
 
+# Selecting the best individual, which is the final solution
 better = None
 bf = float('inf')
 for p in pop:
@@ -161,6 +179,10 @@ for p in pop:
 		better = p
 
 
+## After processing the algorithm, now outputting it ##
+
+
+# Printing the solution
 print ' route:'
 print 'depot'
 for nodeIdx in better:
